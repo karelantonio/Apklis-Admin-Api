cf0dc2568acc010f0b7eae04672f8511bfc282a8
karelantonio <70607434+karelantonio@users.noreply.github.com>
2021-09-14 21:05:59 -0400

    Create InternalServerException.java

---
+++ cu/kareldv/apklis/api2/exceptions/InternalServerException.java
@@ -0,0 +1,23 @@
+package cu.kareldv.apklis.api2.exceptions;
+
+public class InternalServerException extends Error{
+
+    public InternalServerException() {
+    }
+
+    public InternalServerException(String message) {
+        super(message);
+    }
+
+    public InternalServerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InternalServerException(Throwable cause) {
+        super(cause);
+    }
+
+    public InternalServerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
